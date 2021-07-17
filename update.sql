132741ca5e5bf5904e5ce5adeaa6981964fcda10
AaEzha <aaezha@gmail.com>
2021-07-17 13:30:53 +0700

    Update SQL

--- update.sql
+++ update.sql
@@ -1,3 +1,3 @@
 alter table pelanggan add address varchar(255) default "Pandam, Kp. Ambacang" after nik;
-alter table payment add alasan varchar(255);
+alter table payment add alasan text;
 alter table kas_harian modify ket text;
